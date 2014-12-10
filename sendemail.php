8cceee500baac6c1f8cf2db791f2836743d46300
Ahmed Goş <gosahmet@gmail.com>
2014-12-10 16:15:43 +0200

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -11,7 +11,7 @@
     $message = @trim(stripslashes($_POST['message'])); 
 
     $email_from = $email;
-    $email_to = 'gosahmet@gmail.com';//replace with your email
+    $email_to = 'gosahmet@gmail.com';
 
     $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
